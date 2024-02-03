5c9d94bbb55f98dd8a607bdaecd57855847d0203
songhyunjung <sss3593@naver.com>
2024-02-03 19:08:51 +0900

    article

--- appmain/article/routes.py
+++ appmain/article/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, send_from_directory, make_response, jsonify, request
+from flask import Blueprint, send_from_directory, make_response, jsonify, request, url_for
 import sqlite3
 
 from appmain import app
@@ -100,8 +100,69 @@ def getRecentArticles():
 
     return make_response(jsonify(payload), 200)
 
+@article.route('/display_article/<int:articleNo>', methods=['GET'])
+def displayArticlePage(ariticleNo):
+    return send_from_directory(app.root_path, 'templates/display_article.html')
 
+def translateCategory(catId):
+    category = '미분류'
+    
+    if catId == 0:
+        category = '인문'
+    elif catId == 1:
+        category = '사회과학'
+    elif catId == 2:
+        category = '자연과학'
+    elif catId == 3:
+        category = '의학'
+    elif catId == 4:
+        category = '경제/경영'
+    elif catId == 5:
+        category = '공학'
+    elif catId == 6:
+        category = '음악'
+    elif catId == 7:
+        category = '미술'
+    elif catId == 8:
+        category = '기타'
+    else:
+        category = '미분류'
 
+    return category
+
+@article.route('/api/article/display', methods=['GET','POST'])
+def displayArticle():
+
+    data = request.form
+    articleNo = data.get("articleNo")
+
+    payload = {"success": False}
+
+    conn = sqlite3.connect('pyBook.db')
+    cursor = conn.cursor()
+
+    if cursor:
+        SQL = 'SELECT author, title, category, description, price, picture FROM articles WHERE articleNo=?'
+        cursor.execute(SQL, (articleNo,))
+        result = cursor.fetchone()
+
+        cursor.close()
+    conn.close()
+
+    if result:
+        if result[5]:
+            picFilePath = 'pics/' + result[0] + '/' + result[5]
+            picURL = url_for('static', filename=picFilePath, _external=True)
+        else:
+            picURL = None
+
+        article = {"author": result[0], "title": result[1], "category": translateCategory(result[2]),
+                   "description": result[3], "price": result[4], "picture": picURL}
+        payload = {"success": True, "article": article}
+    else:
+        payload = {"success": True, "article": None}
+
+    return make_response(jsonify(payload), 200)
 
 
 
