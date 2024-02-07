963599497bbcf2e7f5889fb3d3638927c53ff51e
songhyunjung <sss3593@naver.com>
2024-02-07 15:42:35 +0900

    reply

---
+++ appmain/reply/__init__.py
@@ -0,0 +1,11 @@
+import sqlite3
+
+conn = sqlite3.connect('pyBook.db')
+cursor = conn.cursor()
+
+SQL = 'CREATE TABLE IF NOT EXISTS replies (replyNo INTEGER PRIMARY KEY AUTOINCREMENT, author TEXT NOT NULL, description TEXT NOT NULL, targetArticle INTEGER NOT NULL)'
+
+cursor.execute(SQL)
+
+cursor.close()
+conn.close()
